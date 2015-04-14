8c149fb03f84ad3774475800d1bdb1cea59fade2
Emily Brenneke <brenneke@gmail.com>
2015-04-14 08:10:50 -0500

    change default, disable registration

--- models/dubstate.js
+++ models/dubstate.js
@@ -10,7 +10,7 @@ var DUBSTATES = {
   YES : {name: "YEP", description: "You are too late, they sold out." }
 };
 
-var currentState = DUBSTATES.NO;
+var currentState = DUBSTATES.MAYBE;
 
 var trigger = function(action) {
   var acted = false;
